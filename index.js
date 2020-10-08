1e282a2582eb254a9a3ec51a94486b6ab0cf52c7
kms0201 <kmskmskmskgb@gmail.com>
2020-10-09 00:59:55 +0900

    #2.6

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const title = document.getElementById("title");
-title.innerHTML = "Hi DOM! from JS";
-title.style.color = "red";
-console.dir(document); // dir 매서드는 원하는 객체의 매서드를 모두 보여줌(console.log)
+function handleClick() {
+  if (title.style.color === "red") {
+    title.style.color = "blue";
+  } else {
+    title.style.color = "red";
+  }
+}
+title.addEventListener("click", handleClick); // dir 매서드는 원하는 객체의 매서드를 모두 보여줌(console.log)

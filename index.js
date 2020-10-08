400e016e53b463424f2cea7e040cafe578327ae7
kms0201 <kmskmskmskgb@gmail.com>
2020-10-09 00:41:59 +0900

    #2.3

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const title = document.getElementById("title");
 title.innerHTML = "Hi DOM! from JS";
+title.style.color = "red";
+console.dir(document); // dir 매서드는 원하는 객체의 매서드를 모두 보여줌(console.log)

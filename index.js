eef6ccdd7e448063f8ca54321309e4e02eec2696
kms0201 <kmskmskmskgb@gmail.com>
2020-10-09 20:01:58 +0900

    #2.7

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
 
-const BASE_COLOR = "rgb(9, 132, 227)";
-const OTHER_COLOR = "rgb(0, 184, 148)";
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
-  console.log(title.style.color);
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if (!hasClass) {
+  //   title.classList.add(CLICKED_CLASS);
+  // } else {
+  //   title.classList.remove(CLICKED_CLASS);
+  // }
+  title.classList.toggle(CLICKED_CLASS); //other way
 }
-
 function init() {
-  title.style.color = BASE_COLOR;
-  title.addEventListener("mouseenter", handleClick);
+  title.addEventListener("click", handleClick);
 }
 init();

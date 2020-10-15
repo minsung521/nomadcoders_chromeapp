70f1c92e14e2b5b1eba936a4efd4c0902239f6d1
kms0201 <kmskmskmskgb@gmail.com>
2020-10-15 10:35:21 +0900

    #3.10 JS finished

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather")
+
 const COORDS = 'coords';
+const API_KEY = "c2844939ad8b05a45f495e379a796327";
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSuccess(position){
         longitude//longitude : longitude
     };
     saveCoords(coordsObj);    
+    getWeather(latitude,longitude);
 }
 function handleGeoError(){
     console.log("cannot access geo location");
@@ -25,7 +41,9 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     }else{
-        
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
     }
 }
 function init(){
